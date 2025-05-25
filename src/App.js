0b9560958f09a0a7f2bc130db20639b72ba13720
Pilar <pilarchia.14@gmail.com>
2025-05-25 13:40:56 -0400

    Added banner to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg'
 import './App.css';
 import {NavBar} from './components/NavBar';
+import {Banner} from './components/Banner';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Banner/>
     </div>
   );
 }
